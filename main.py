22d2dd0622f70183657025a8165492aa46267a48
JULIUS8056 <juliusolasunmibo321@gmail.com>
2024-01-26 13:25:39 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import catboost
+st.set_page_config(
+    page_title="Home Home",
+    layout="centered"
+)
+st.title("Diabetes Prediction System")
+st.sidebar.success("Select your preffered language.")
